d36cbbc69baab7c41f64396c0be38612f2a541b2
manojbandari <bandarimanoj22@msitprogram.net>
2018-11-05 19:19:36 +0530

    "ads2_m6_a1_1: 3/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m6/ADS-2Week-1/Solution.java
+++ assignments/m6/ADS-2Week-1/Solution.java
@@ -73,7 +73,7 @@ public class Solution {
 			}
 		}
 
-		System.out.println(d);
+		//System.out.println(d);
 		for (int i = 0; i < vertices; i++) {
 			if (d.outdegree(i) == 0) {
 				for (int j = 0; j < vertices; j++) {
