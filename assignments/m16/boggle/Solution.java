6b587f712d8a09b0c7eba96502f5326e73349ee6
manojbandari <bandarimanoj22@msitprogram.net>
2018-11-16 02:09:03 +0530

    "ads2_m16_a1_1: 29/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m16/boggle/Solution.java
+++ assignments/m16/boggle/Solution.java
@@ -40,7 +40,11 @@ public class Solution {
 			/*for (String word : solver.getAllValidWords(board)) {
 				score += solver.scoreOf(word);
 			}*/
-			StdOut.println("Score = " + score);
+			// StdOut.println("Score = " + score);
+			if (board == null) {
+					System.out.println("board is null");
+
+				} else StdOut.println("Score = " + score);
 			break;
 
 		default:
